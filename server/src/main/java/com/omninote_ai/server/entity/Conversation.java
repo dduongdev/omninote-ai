115571d0c4af18ea05c45afbc42fd59d3bc9da19
dduongdev <nguyendongduong4551190010@gmail.com>
2026-03-15 20:13:56 +0700

    Set event ack-mode manual

--- server/src/main/java/com/omninote_ai/server/entity/Conversation.java
+++ server/src/main/java/com/omninote_ai/server/entity/Conversation.java
@@ -4,11 +4,9 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
